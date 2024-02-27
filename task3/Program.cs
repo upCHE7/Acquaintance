755a43150c3dab212c850e186b7d864c8ee6e52f
upCHE7 <136953998+upCHE7@users.noreply.github.com>
2024-02-27 19:42:35 +0300

    Новый коммит5

--- task3/Program.cs
+++ task3/Program.cs
@@ -10,3 +10,18 @@ Console.WriteLine(sum1);
 double d = 7.7;
 double sum2 = l + d;
 Console.WriteLine(sum2);
+
+int product = i * i1;
+long product1 = i * l;
+double product2 = i * d;
+Console.WriteLine(product);
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+
+int quotient = i / i;
+Console.WriteLine(quotient);
+long quo = l / i1;
+Console.WriteLine(quo);
+
+double quo1 = l / d;
+Console.WriteLine(quo1);
