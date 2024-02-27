b6971fbfb35fe2454c134676f62c62d7301e65bc
upCHE7 <136953998+upCHE7@users.noreply.github.com>
2024-02-27 19:16:07 +0300

    Новый коммит4

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,3 +3,10 @@ int i1 = 2;
 int sum = i + i1;
 Console.WriteLine(sum);
 
+long l = 798796765764654;
+long sum1 = l + i1;
+Console.WriteLine(sum1);
+
+double d = 7.7;
+double sum2 = l + d;
+Console.WriteLine(sum2);
