5118b341ac14c3835b86cf935dbf73eeaa5c4074
upCHE7 <136953998+upCHE7@users.noreply.github.com>
2024-02-28 10:41:10 +0300

    Новый коммит7

---
+++ task5/Program.cs
@@ -0,0 +1,31 @@
+﻿int a = 77;
+int b = 5;
+int c = 3;
+int d = 47;
+int e = 4;
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+else max = a;
+
+if (c > max)
+{
+    max = c;
+}
+else max = a;
+
+if (d > max)
+{
+    max = d;
+}
+else max = a;
+if (e > max)
+{
+    max = e;
+}
+else max = a;
+
+Console.WriteLine(max);
